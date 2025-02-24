e8b98452fdc598b29de84835f0721db01d7d5908
marco-bernal <bernal.marco.a.84@gmail.com>
2025-02-24 17:31:33 -0600

    MCA-3 - Add flyway migrations and spring test_data profile

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -1,13 +1,16 @@
---TODO: Research for a dataset in kaggle
+-- Incident_Rate: Incidence Rate = cases per 100,000 persons.
+-- Case_Fatality_Ratio (%): Case-Fatality Ratio (%) = Number recorded deaths / Number cases.
 CREATE TABLE public.covid
 (
     id SERIAL NOT NULL,
     country character varying(70) NOT NULL UNIQUE,
     continent character varying(50) NOT NULL UNIQUE,
+    confirmed integer NOT NULL,
+    death integer NOT NULL,
+    recovered integer NOT NULL,
     active integer NOT NULL,
-    dead integer NOT NULL,
-    recovered integer NOT NULL
     created_at timestamp default CURRENT_TIMESTAMP,
     updated_at timestamp default CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
+
