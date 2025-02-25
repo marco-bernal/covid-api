c5cd867b3164293640717af6731db48ba9fc2888
marco-bernal <bernal.marco.a.84@gmail.com>
2025-02-25 10:13:38 -0600

    MCA-4 - Fix imports

--- src/main/java/com/covid/api/model/Report.java
+++ src/main/java/com/covid/api/model/Report.java
@@ -1,7 +1,6 @@
 package com.covid.api.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +8,6 @@ import lombok.ToString;
 
 @Getter
 @Setter
-//@Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
